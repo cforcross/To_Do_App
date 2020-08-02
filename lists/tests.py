30940a34bafdf2bd18efafc3daba6976c767033e
cforcross <61464091+cforcross@users.noreply.github.com>
2020-08-03 02:57:38 +0300

    'basic_view_returns_minimal_html'

--- lists/tests.py
+++ lists/tests.py
@@ -2,6 +2,7 @@ from django.test import TestCase
 from django.test import TestCase
 from .views import home_page
 from django.urls import resolve
+from django.http import HttpRequest
 # Create your tests here.
 class SmokeTest(TestCase):
     # def test_bad_maths(self):
@@ -10,3 +11,10 @@ class SmokeTest(TestCase):
     def test_root_url_resolves_to_home_page_view(self):
         found = resolve('/') #maps django urls
         self.assertEqual(found.func,home_page)
+        
+    def test_home_page_returns_correct_html(self):
+        request = HttpRequest()
+        response = home_page(request)
+        self.assertTrue(response.content.startswith(b'<html>'))
+        self.assertIn(b'<title>To-Do</title>',response.content)
+        self.assertTrue(response.content.endswith(b'</html>'))
