e5f436695e9271af0dfd7682f2084ead98973bf5
cforcross <61464091+cforcross@users.noreply.github.com>
2020-08-03 02:18:07 +0300

    'first'

--- lists/tests.py
+++ lists/tests.py
@@ -1,6 +1,12 @@
 from django.test import TestCase
-
+from django.test import TestCase
+from .views import home_page
+from django.urls import resolve
 # Create your tests here.
 class SmokeTest(TestCase):
-    def test_bad_maths(self):
-        self.assertEqual(1+1,3)
+    # def test_bad_maths(self):
+    #     self.assertEqual(1+1,3)
+        
+    def test_root_url_resolves_to_home_page_view(self):
+        found = resolve('/') #maps django urls
+        self.assertEqual(found.func,home_page)
